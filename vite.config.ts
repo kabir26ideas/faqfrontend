508f212604212a650df0fc9a28b9c35f2c7f5860
kabir26ideas <kabir.seth@26ideas.com>
2025-07-15 12:49:44 +0530

    updating vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/thinkificbackend/',
+  base: '/faqfrontend/',
   plugins: [
     react(),
     mode === 'development' &&
